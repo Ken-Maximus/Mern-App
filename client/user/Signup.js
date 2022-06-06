b860687fe795268d343d2c12dea95741343d490d
Ken-Maximus <micaiah4life@gmail.com>
2022-06-05 20:01:32 -0600

    commit

--- client/user/Signup.js
+++ client/user/Signup.js
@@ -59,8 +59,8 @@ export default function Signup (){
     const user = {
       name: values.name || undefined,
       email: values.email || undefined,
-      password: values.password || undefined
-    
+      password: values.password || undefined,
+      cpassword: values.cpassword || undefined,
 
     }
     create(user).then((data) => {
@@ -81,7 +81,7 @@ export default function Signup (){
           <TextField id="name" label="Name" className={classes.textField} value={values.name} onChange={handleChange('name')} margin="normal"/><br/>
           <TextField id="email" type="email" label="Email" className={classes.textField} value={values.email} onChange={handleChange('email')} margin="normal"/><br/>
           <TextField id="password" type="password" label="Password" className={classes.textField} value={values.password} onChange={handleChange('password')} margin="normal"/>
-          <TextField id="password" type=" password" label="Confirm Password" className={classes.textField} value={values.password} onChange={handleChange('password')} margin="normal"/>
+          <TextField id="cpassword" type="cpassword" label="Confirm Password" className={classes.textField} value={values.cpassword} onChange={handleChange('cpassword')} margin="normal"/>
 
           <br/> {
             values.error && (<Typography component="p" color="error">
